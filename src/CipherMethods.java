8ef6307f8f29d332c9f6bffeeb9f7ca6c2332834
Daniel Shafer <daniel.shafer@outlook.com>
2024-02-05 13:44:16 -0500

    updating FileHandler and CipherMethods classes

--- src/CipherMethods.java
+++ src/CipherMethods.java
@@ -1,2 +1,20 @@
 public class CipherMethods {
+    String plainText;
+    String cipherText;
+
+    //Constructors
+    public CipherMethods(){
+
+    }
+
+    //Method Declarations
+    public void Encrypt(){
+
+    }
+    public void Decrypt(){
+
+    }
+    public void compareToDict(){
+
+    }
 }
