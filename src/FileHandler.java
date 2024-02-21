8e19e72cedae9f64f8587f390acf67787664f5f4
Daniel Shafer <daniel.shafer@outlook.com>
2024-02-21 17:06:58 -0500

    Added documentation to CipherMethods.java class for readability

---
+++ src/FileHandler.java
@@ -0,0 +1,26 @@
+public class FileHandler {
+    int shiftNumber;
+    private int[] keySpace = new int[26];
+
+    //Constructors
+    public FileHandler(int shift){
+        shiftNumber = shift;
+    }
+
+    //Method Declarations
+    public void loadPlain(){
+
+    }
+    public void loadCipher(){
+
+    }
+    public void loadDict(){
+
+    }
+    public void parseCipher(){
+
+    }
+    public void saveCipher(){
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean endFlag = false;
+        while (!endFlag) {
+            System.out.println("Welcome to Team Kida's Shift Cipher! \nEnter exit to end.\n");
+            Scanner inputObject = new Scanner(System.in);
+            System.out.println("1. Encrypt a String \n2. Decrypt a String \n3. End");
+            int menuInput = inputObject.nextInt();
+            if (menuInput == 1) {
+                inputObject = new Scanner(System.in);
+                System.out.println("Enter plaintext to be encrypted:");
+                String plainText = inputObject.nextLine();
+                inputObject = new Scanner(System.in);
+                System.out.println("Enter encryption key (positive integer 0 to 25):");
+                int key = inputObject.nextInt();
+                if (0 <= key && key <= 25) {
+                    CipherMethods newCipher = new CipherMethods(key);
+                    String cipherText = newCipher.Encrypt(plainText, key);
+                    System.out.println("Encrypted ciphertext: " + cipherText);
+                } else {
+                    System.out.println("error, key outside bounds \n");
+                }
+            } else if (menuInput == 2) {
+                inputObject = new Scanner(System.in);
+                System.out.println("Enter ciphertext to be decrypted:");
+                String cipherText = inputObject.nextLine();
+                inputObject = new Scanner(System.in);
+                System.out.println("Enter encryption key (positive integer 0 to 25):");
+                int key = inputObject.nextInt();
+                if (0 <= key && key <= 25) {
+                    CipherMethods newCipher = new CipherMethods(key);
+                    String plainText = newCipher.Decrypt(cipherText, key);
+                    System.out.println("Decrypted plaintext: " + plainText);
+                } else {
+                    System.out.println("error, key outside bounds \n");
+                }
+            } else if (menuInput == 3) {
+                endFlag = true;
+            } else {
+                System.out.println("Error, please try again.");
+            }
+        }
+    }
+}
