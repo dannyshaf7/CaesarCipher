fd16595fdc27452e39377f5ba9266b4efc546afa
Daniel Shafer <daniel.shafer@outlook.com>
2024-02-21 17:05:24 -0500

    Added documentation to CipherMethods.java class for readability

--- src/CipherMethods.java
+++ src/CipherMethods.java
@@ -1,17 +1,38 @@
 import java.lang.Math;
 
 public class CipherMethods {
+
+    // Class Variable Declarations:
     private String plainText;
     private String cipherText = "";
-    int cipherKey;
-    private char[] alphaPos = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private int cipherKey;
+    // array of chars - english alphabet - used for caesar cipher position comparisons
+    private char[] alphaPos = new char[]{'a','b','c','d','e','f','g','h','i','j','k',
+            'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-    //Constructors
+    // Default Constructor
+    public CipherMethods() {
+        this(0);
+    }
+    // Parameterized Constructor
     public CipherMethods(int key){
         cipherKey = key;
     }
 
     //Method Declarations
+
+    /* Encrypt method: takes string plaintext and int key as parameters
+        charPos holds the position of the character in the alphaPos array
+        plainChar holds the current plaintext char to be compared to the current
+            cipherChar ciphertext char
+        first for-loop iterates plaintext, adding whitespace to ciphertext when found
+        else for-loop iterates through alphaPos array
+            'if' looks for a match between plainChar and cipherChar, defines charPos as j
+                if cipherKey is less than 0, we use the decryption formulas
+                    java does not handle negative number mod 26 like we want
+                else we use the standard modulo function
+        method returns ciphertext as String
+    */
     public String Encrypt(String text, int key){
         plainText = text;
         cipherKey = key;
@@ -29,11 +50,19 @@ public class CipherMethods {
                     if (plainChar == cipherChar) {
                         charPos = j;
                         if (cipherKey < 0){
-                            double negPos = charPos + cipherKey;  // 2 - 5 = -3
-                            int floorVal = (int)Math.floor(negPos / 26); // -3/26 = -0.1153846
-                            charPos = (int)negPos - (26 * floorVal); // -3 - (-26) = 23
+                            // found to get a double from dividing, must start with double negPos
+                            double negPos = charPos + cipherKey;
+                            // we find the next lowest multiple of 26 by dividing the negative
+                            // position value by the mod value 26, calculating the floor to
+                            // reach the needed integer value, and multiplying it back to the
+                            // mod value 26; then, subtracting 26*floorVal a negative int from
+                            // the negPos will effectively subtract from 26 to get decrypt position
+                            int floorVal = (int)Math.floor(negPos / 26);
+                            charPos = (int)negPos - (26 * floorVal);
                         }
                         else{
+                            // adding the current position and key then mod 26 will give the
+                            // correct encrypt position, even when it goes beyond z back to a
                             charPos = (charPos + key) % 26;
                         }
                         cipherChar = alphaPos[charPos];
@@ -44,6 +73,12 @@ public class CipherMethods {
         }
         return cipherText;
     }
+
+    /* Decrypt method: takes String ciphertext and int key as parameters
+        makes the key negative and calls the encrypt function, passing the ciphertext and
+        key (now a negative int) as parameters
+        method returns the plaintext as String
+     */
     public String Decrypt(String text, int key){
         cipherKey = -1 * key;
         return Encrypt(text, cipherKey);
