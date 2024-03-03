29a2a8111363a3da7a293fc3b8d40d30cac3d4ce
Daniel Shafer <daniel.shafer@outlook.com>
2024-03-03 00:01:29 -0500

    Added Kiara's code for FileHandler, did not run when copying over, worked out bugs and now running load to encrypt and load to decrypt

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,26 +1,50 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class FileHandler {
-    int shiftNumber;
-    private int[] keySpace = new int[26];
 
     //Constructors
-    public FileHandler(int shift){
-        shiftNumber = shift;
+    public FileHandler(){
+
     }
 
     //Method Declarations
-    public void loadPlain(){
+    public ArrayList<String> loadFile (String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        Scanner fileInput = new Scanner(path);
+        fileInput.useDelimiter(" ");
 
+        ArrayList<String> plainText = new ArrayList<>();
+        while (fileInput.hasNext()){
+            String word = fileInput.next();
+            plainText.add(word);
+        }
+        return plainText;
     }
-    public void loadCipher(){
-
-    }
-    public void loadDict(){
-
-    }
-    public void parseCipher(){
-
-    }
-    public void saveCipher(){
-
+    public void writeText(String string) throws IOException {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please input the name of the file");
+        String filename = userInput.nextLine();
+        File file = new File(filename); //Should be absolute path
+        if (file.createNewFile()){
+            FileWriter writer = new FileWriter(filename);
+            for (int i=0; i<string.length(); i++){
+                writer.write(string.charAt(i));
+            }
+        }
+        else{
+            System.out.println("The file already exists");
+        }
     }
 }
